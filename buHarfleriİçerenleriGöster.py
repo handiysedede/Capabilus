4625af88165d1c105e762682e4b7df9f173fa2d9
handiysedede <62362796+handiysedede@users.noreply.github.com>
2020-07-16 21:14:19 +0300

    Add files via upload
    Abimin algoritması

---
+++ buHarfleriİçerenleriGöster.py
@@ -0,0 +1,7 @@
+liste=["YEM", "İLE", "İLKE", "ÜLKE", "EKİM", "KEM", "İLME", "YEK", "LEY", "ÜYE", "MELİK", "KÜME", "EKLİ", "MÜLKİ", "MÜLK", "ELİM", "KÜMELİ", "YÜKLEM", "YEMLİK", "MEYİL", "LİM", "LÜK", "LİME", "MİL", "ELİK", "ELK", "İYE", "KİLE", "İLEK", "EMLİK", "İMLEK", "EMİK", "MÜL", "EKÜ"]
+
+
+for li in liste:
+    if li[1]=="E" and li[2]==:
+        print(li)
+        
