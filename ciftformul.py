e5e7133323f3b5b4a16fa84143a0eb6ab582d26f
handiysedede <62362796+handiysedede@users.noreply.github.com>
2020-07-22 14:23:33 +0300

    Add files via upload

---
+++ ciftformul.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 22 13:40:24 2020
+
+@author: golge
+"""
+my_list=[]
+for x in range(1,10):
+    if x % 2 ==0:
+        my_list.append(x)
+for my_listİtem in my_list:
+    if my_listİtem%2==0:
+        print(my_listİtem)
+    else:
+        print(my_listİtem)
